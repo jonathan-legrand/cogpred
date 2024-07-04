1c3e28eeb3291a902acbe24b35d3c420f8486235
Jonathan Legrand <jonathanamlegrand@gmail.com>
2024-07-04 15:54:05 +0200

    parametrable CNN with conv blocks

--- cogpred/models.py
+++ cogpred/models.py
@@ -3,6 +3,22 @@ import numpy as np
 from skorch import NeuralNetClassifier
 from torch import nn
 
+from prettytable import PrettyTable
+
+def count_parameters(model):
+    table = PrettyTable(["Modules", "Parameters"])
+    total_params = 0
+    for name, parameter in model.named_parameters():
+        if not parameter.requires_grad:
+            continue
+        params = parameter.numel()
+        table.add_row([name, params])
+        total_params += params
+    print(table)
+    print(f"Total Trainable Params: {total_params}")
+    return total_params
+    
+
 class WindowNetClassifier(NeuralNetClassifier):
     def sliding_inference(self, segment, stride=4, k=3):
         n_TR = segment.shape[-1] #  # CNN expects (N, C_in, L)
