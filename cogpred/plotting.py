d9fc18a8fa3bd54439e51b07afe632fae44dbf0d
Jonathan Legrand <jonathanamlegrand@gmail.com>
2024-07-12 10:15:10 +0200

    Add input features to inference plot

--- cogpred/plotting.py
+++ cogpred/plotting.py
@@ -1,22 +1,24 @@
 import matplotlib.pyplot as plt
 
-def plot_ts(ts):
+def plot_ts(ts, ax=None):
     """
     ts: bold time series of shape (n_TR, n_regions)
     """
     from scipy.stats import zscore
-    fig, ax = plt.subplots(figsize=(8, 4))
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(8, 4))
     ts = zscore(ts)
     ax.plot(ts)
     ax.set_xlabel("TR")
     ax.set_ylabel("Signal intensity")
-    return fig
+    return ax
 
-def plot_predictions(p):
-    fig, ax = plt.subplots(figsize=(8, 4))
+def plot_predictions(p, ax=None):
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(8, 4))
     ax.plot(p.T, label=["Stable", "Slow decline", "Steep decline"])
     ax.set_xlabel("TR")
     ax.set_ylabel("Model output")
     ax.set_ylim(0, 1)
-    fig.legend()
-    return fig
+    ax.legend()
+    return ax
