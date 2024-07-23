c1d72551c12ffee5d8b6a41c6e3f791dfa3f3471
Jonathan Legrand <jonathanamlegrand@gmail.com>
2024-07-23 16:35:29 +0200

    Parcellation inside the DMN

--- parcellation.py
+++ parcellation.py
@@ -1,3 +1,4 @@
+# %%
 
 from pathlib import Path
 import os
@@ -22,50 +23,66 @@ conn_dir = config["connectivity_matrices"]
 ATLAS = "msdl" # We don't really care about the atlas
 k = 3
 _, metadata = make_training_data(conn_dir, ATLAS, k)
-func_filenames = metadata.groupby("CEN_ANOM")["file_path"].sample(n=1, random_state=1234).to_list()
+func_filenames = metadata["file_path"].sample(n=100, random_state=1234).to_list()
+# %%
 
 clean_kwargs = {
-    "clean__strategy": ["high_pass", "wm_csf", "motion", "global_signal"]
+    "clean__strategy": ["high_pass", "wm_csf", "motion"]
 }
 
-N_COMPONENTS = 30
+N_COMPONENTS = 10
+
+from nilearn.image import binarize_img, resample_to_img
+mask_img = nib.load(Path(config["data_dir"]) / "default_association-test_z_FDR_0.01.nii.gz")
+mask_img = resample_to_img(mask_img, func_filenames[0])
+mask_img = binarize_img(mask_img, 0.1, two_sided=False)
+# TODO Resample mask image to the images' resolution
+# %%
 
 mask = MultiNiftiMasker(
+    mask_img,
     smoothing_fwhm=6.0,
-    mask_strategy="epi", # TODO Limit to EPI?
+    mask_strategy="epi",
     standardize="zscore_sample",
-    n_jobs=10,
     verbose=1,
     memory_level=2,
     **clean_kwargs
 )
 
 dict_learning = DictLearning(
-    n_components=N_COMPONENTS, # Debug config
+    n_components=N_COMPONENTS,
     mask=mask,
     memory="nilearn_cache",
     memory_level=2,
     random_state=1234,
     verbose=1,
+    n_jobs=10
 )
 
 dict_learning.fit(func_filenames)
 
 components_img_ = dict_learning.components_img_
 
+# %%
 extractor = RegionExtractor(
 	maps_img=components_img_,
-	extractor='local_regions'
+	extractor='local_regions',
+    min_region_size=900, # in mm^3
 )
 extractor.fit()
 regions_extracted_img = extractor.regions_img_
 regions_index = pd.Series(extractor.index_)
 n_regions_extracted = regions_extracted_img.shape[-1]
 
+plot_prob_atlas(
+    regions_extracted_img, view_type="filled_contours"
+)
 
+#%%
 run_path = Path(config["output_dir"]) / "parcellations" / make_run_name(
     ncomponents=N_COMPONENTS,
     nregions=n_regions_extracted,
+    gsr=False
 )
 os.makedirs(run_path, exist_ok=True)
 
@@ -77,3 +94,5 @@ plot_prob_atlas(
 )
 plt.savefig(run_path / "parcellation.png")
 print(f"Run saved in {run_path}")
+
+# %%
